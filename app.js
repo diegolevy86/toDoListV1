4c74ed444e674aeb68990be366073ffb18c11b89
Diego Levy <dielevy@gmail.com>
2023-05-29 09:43:56 -0300

    last changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname +"/date.js");
 const port = 3010;
 
 const app = express();
@@ -9,14 +10,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let items = ["Study", "Do the dishes", "Shower", "Cook", "Eat"];
-let workItems = [];
+const items = ["Study", "Do the dishes", "Shower", "Cook", "Eat"];
+const workItems = [];
 
 app.get("/", function(req, res){
-    let today = new Date();
-    /*let dayNumber = today.getDay();*/
-    let options = {weekday: "long", day: "numeric", month: "long"};
-    let day = today.toLocaleDateString("en-US", options);
 
     /*switch(dayNumber)
     {
@@ -46,6 +43,7 @@ app.get("/", function(req, res){
             break;
     }*/
 
+    let day = date.getDate();
     res.render("list", {listTitle: day, newItems: items});
 });
 
@@ -66,6 +64,10 @@ app.post("/", function(request, response){
 app.get("/work", function(req, res){
     res.render("list", {listTitle: "Work List", newItems: workItems});
  
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
 });
 
 app.listen(port, function(){
